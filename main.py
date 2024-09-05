2cdabc18f33359d4424632894a909934192f5853
Brometheus115 <robert359960@hotmail.de>
2024-09-05 09:21:35 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# statically type language JAVA
+
+# int age = 95
+
+# a dynamicaly typed language PYTHON
+
+# there is no need to define what type of object the variabel must hold
+
+age:int = 57
+name:int = 98
+country:str = "Spain" 'G'
+# print
+# function_name (value1, value2, value3)
+# print send to the console/terminal
